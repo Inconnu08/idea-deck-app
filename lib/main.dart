aa4f4debf6664ad957ec17263aa1dbd4adac39e9
Inconnu08 <taufiqrx8@gmail.com>
2021-03-04 15:19:03 +0600

    Refactor to a better player (Pun intended)

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
-import 'package:chewie/chewie.dart';
+import 'package:better_player/better_player.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:video_player/video_player.dart';
 
 import './routes.dart';
 import './screens/splash.dart';
 import './size_config.dart';
 import './theme.dart';
+import 'constants.dart';
 
 main() {
   // debugPaintSizeEnabled=true;
@@ -49,165 +49,145 @@ class ChewieDemo extends StatefulWidget {
 }
 
 class _ChewieDemoState extends State<ChewieDemo> {
-  TargetPlatform _platform;
-  VideoPlayerController _videoPlayerController1;
-  VideoPlayerController _videoPlayerController2;
-  ChewieController _chewieController;
+  BetterPlayerController _betterPlayerController;
 
   @override
   void initState() {
-    super.initState();
-    initializePlayer();
-  }
-
-  @override
-  void dispose() {
-    _videoPlayerController1.dispose();
-    _videoPlayerController2.dispose();
-    _chewieController?.dispose();
-    super.dispose();
-  }
-
-  Future<void> initializePlayer() async {
-    _videoPlayerController1 = VideoPlayerController.network(
-        'https://assets.mixkit.co/videos/preview/mixkit-forest-stream-in-the-sunlight-529-large.mp4');
-    _videoPlayerController2 = VideoPlayerController.network(
-        'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1226-large.mp4');
-    await Future.wait([
-      _videoPlayerController1.initialize(),
-      _videoPlayerController2.initialize()
-    ]);
-    _chewieController = ChewieController(
-      videoPlayerController: _videoPlayerController1,
-      autoPlay: true,
-      looping: true,
-      // Try playing around with some of these other options:
-
-      // showControls: false,
-      // materialProgressColors: ChewieProgressColors(
-      //   playedColor: Colors.red,
-      //   handleColor: Colors.blue,
-      //   backgroundColor: Colors.grey,
-      //   bufferedColor: Colors.lightGreen,
-      // ),
-      // placeholder: Container(
-      //   color: Colors.grey,
-      // ),
-      // autoInitialize: true,
+        BetterPlayerControlsConfiguration controlsConfiguration =
+        BetterPlayerControlsConfiguration(
+      controlBarColor: Colors.white,
+      iconsColor: kPrimaryColor,
+      progressBarPlayedColor: buttonColor,
+      progressBarHandleColor: buttonColor,
+      enableSkips: false,
+      enableFullscreen: true,
+      enableProgressBarDrag: false,
+      loadingColor: kAccent,
+      overflowModalTextColor: Colors.white,
+      overflowMenuIconsColor: Colors.white,
     );
-    setState(() {});
+
+    BetterPlayerConfiguration betterPlayerConfiguration =
+        BetterPlayerConfiguration(
+            aspectRatio: 16 / 9,
+            fit: BoxFit.contain,
+            autoDetectFullscreenDeviceOrientation: true,
+            controlsConfiguration: controlsConfiguration
+            );
+    BetterPlayerDataSource dataSource = BetterPlayerDataSource(
+      BetterPlayerDataSourceType.network,
+      "https://res.cloudinary.com/dxisrwiao/video/upload/v1613809156/e0ulp3n1glxioxho7ubt.m3u8",
+    );
+    _betterPlayerController = BetterPlayerController(betterPlayerConfiguration);
+    _betterPlayerController.setupDataSource(dataSource);
+    super.initState();
   }
+  // VideoPlayerController _videoPlayerController1;
+  // ChewieController _chewieController;
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   initializePlayer();
+  // }
+
+  // @override
+  // void dispose() {
+  //   _videoPlayerController1.dispose();
+  //   _chewieController?.dispose();
+  //   super.dispose();
+  // }
+
+  // Future<void> initializePlayer() async {
+  //   _videoPlayerController1 = VideoPlayerController.network(
+  //       'https://assets.mixkit.co/videos/preview/mixkit-forest-stream-in-the-sunlight-529-large.mp4');
+
+  //   await _videoPlayerController1.initialize();
+
+  //   _chewieController = ChewieController(
+  //     videoPlayerController: _videoPlayerController1,
+  //     aspectRatio: 16 / 9,
+  //     autoPlay: false,
+  //     looping: false,
+  //     allowedScreenSleep: false,
+  //     allowPlaybackSpeedChanging: false,
+  //     errorBuilder: (context, errorMessage) {
+  //       return Center(
+  //         child: Text(
+  //           "Something went wrong! Please try later.",
+  //           style: TextStyle(color: Colors.white),
+  //         ),
+  //       );
+  //     },
+  //     // Try playing around with some of these other options:
+
+  //     // showControls: false,
+  //     materialProgressColors: ChewieProgressColors(
+  //       playedColor: kPrimaryColor,
+  //       handleColor: kPrimaryColor,
+  //       backgroundColor: lightText,
+  //       bufferedColor: Colors.grey,
+  //     ),
+  //     // autoInitialize: true,
+  //   );
+  //   setState(() {});
+  // }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: widget.title,
-      theme: ThemeData.light().copyWith(
-        platform: _platform ?? Theme.of(context).platform,
-      ),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Column(
-          children: <Widget>[
-            Expanded(
-              child: Center(
-                child: _chewieController != null
-                    ? Chewie(
-                        controller: _chewieController,
-                      )
-                    : Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          CircularProgressIndicator(),
-                          SizedBox(height: 20),
-                          Text('Loading'),
-                        ],
-                      ),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              // _chewieController != null
+              //     ?
+              // ? Container(
+              //   height: 300,
+              //   child: Chewie(
+              //       controller: _chewieController,
+              //     ),
+              // )
+              AspectRatio(
+                aspectRatio: 16 / 9,
+                child: BetterPlayer(controller: _betterPlayerController),
               ),
-            ),
-            TextButton(
-              onPressed: () {
-                _chewieController.enterFullScreen();
-              },
-              child: const Text('Fullscreen'),
-            ),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: TextButton(
-                    onPressed: () {
-                      setState(() {
-                        _chewieController.dispose();
-                        _videoPlayerController1.pause();
-                        _videoPlayerController1.seekTo(const Duration());
-                        _chewieController = ChewieController(
-                          videoPlayerController: _videoPlayerController1,
-                          autoPlay: true,
-                          looping: true,
-                        );
-                      });
-                    },
-                    child: const Padding(
-                      padding: EdgeInsets.symmetric(vertical: 16.0),
-                      child: Text("Landscape Video"),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: TextButton(
-                    onPressed: () {
-                      setState(() {
-                        _chewieController.dispose();
-                        _videoPlayerController2.pause();
-                        _videoPlayerController2.seekTo(const Duration());
-                        _chewieController = ChewieController(
-                          videoPlayerController: _videoPlayerController2,
-                          autoPlay: true,
-                          looping: true,
-                        );
-                      });
-                    },
-                    child: const Padding(
-                      padding: EdgeInsets.symmetric(vertical: 16.0),
-                      child: Text("Portrait Video"),
-                    ),
-                  ),
-                )
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: TextButton(
-                    onPressed: () {
-                      setState(() {
-                        _platform = TargetPlatform.android;
-                      });
-                    },
-                    child: const Padding(
-                      padding: EdgeInsets.symmetric(vertical: 16.0),
-                      child: Text("Android controls"),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: TextButton(
-                    onPressed: () {
-                      setState(() {
-                        _platform = TargetPlatform.iOS;
-                      });
-                    },
-                    child: const Padding(
-                      padding: EdgeInsets.symmetric(vertical: 16.0),
-                      child: Text("iOS controls"),
-                    ),
-                  ),
-                )
-              ],
-            )
-          ],
+              // : Column(
+              //     mainAxisAlignment: MainAxisAlignment.center,
+              //     children: const [
+              //       CircularProgressIndicator(),
+              //       SizedBox(height: 20),
+              //       Text('Loading'),
+              //     ],
+              //   ),
+
+              Row(
+                children: <Widget>[
+                  // TextButton(
+                  //   onPressed: () {
+                  //     setState(() {
+                  //       _chewieController.dispose();
+                  //       _videoPlayerController1.pause();
+                  //       _videoPlayerController1.seekTo(const Duration());
+                  //       _chewieController = ChewieController(
+                  //         videoPlayerController: _videoPlayerController1,
+                  //         autoPlay: true,
+                  //         looping: true,
+                  //       );
+                  //     });
+                  // },
+                  //   child: const Padding(
+                  //     padding: EdgeInsets.symmetric(vertical: 16.0),
+                  //     child: Text("Landscape Video"),
+                  //   ),
+                  // ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
